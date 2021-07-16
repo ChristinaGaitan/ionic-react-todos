ab498fbdab8ff852826e7db2c65094c54552bda7
Christina Gaitan <christina.gaitan@gmail.com>
2021-07-16 13:25:17 -0500

    Add second test

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -6,3 +6,8 @@ test('page should have a title of Ionic React Todos', async () => {
   const { findByText } = render(<Home />)
   await findByText('Ionic React Todos');
 })
+
+test('when there are no todos, a no todos message should show', async() => {
+  const { findByText } = render(<Home />)
+  await findByText('No todos, add some!')
+})
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,16 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
+export interface Todo {
+  id: number;
+  text: string;
+}
+
 const Home: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +25,15 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+      </IonContent>
+      <IonContent>
+        {
+          todos.length === 0 ? (
+            <div>No todos, add some!</div>
+          ) : (
+            <div>todos will go here</div>
+          )
+        }
       </IonContent>
     </IonPage>
   );
